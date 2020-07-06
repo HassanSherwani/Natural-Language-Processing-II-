03ad92dfe0ef0f19f7b7a67ee3cfe4998d45bc2c
HassanSherwani <45789548+HassanSherwani@users.noreply.github.com>
2020-07-06 08:08:03 +0200

    runner file

---
+++ 17.Chatbot_ChatterBot/chatbot.py
@@ -0,0 +1,24 @@
+from chatterbot import ChatBot
+#from chatterbot.trainers import ListTrainer
+
+
+def get_response(usrText):
+    bot = ChatBot('Bot',
+                  storage_adapter='chatterbot.storage.SQLStorageAdapter',
+#    logic_adapters=[
+#        {
+#            "import_path": "chatterbot.logic.BestMatch",
+#            "statement_comparison_function": chatterbot.comparisons.levenshtein_distance,
+#            "response_selection_method": chatterbot.response_selection.get_first_response
+#        }
+#    ],
+    trainer='chatterbot.trainers.ListTrainer')
+    #bot.set_trainer(ListTrainer)
+    while True:
+        if usrText.strip()!= 'Bye':
+            result = bot.get_response(usrText)
+            reply = str(result)
+            return(reply)
+        if usrText.strip() == 'Bye':
+            return('Bye')
+            break
